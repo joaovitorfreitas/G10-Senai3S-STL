3b0e90cd0f1774cde4b93d2c14595d3d8484865a
joaovitorfreitas <joaovdlf@gmail.com>
2021-10-22 03:49:08 -0300

    Login-Cadastro back

---
+++ BD/DDL_OKEntregas.sql
@@ -0,0 +1,6 @@
+USE OKEntregas
+
+INSERT INTO Usuarios	(Nome, Email, Senha, TipoUsuario)
+VALUES                  ('Daniel', 'daniel@gmail.com', '123', 0),
+						('Alan', 'alan@gmail.com', '123', 1)
+							
---
+++ BD/DQL_OKEntregas.sql
@@ -0,0 +1,3 @@
+USE OKEntregas
+
+SELECT * FROM Usuarios;
