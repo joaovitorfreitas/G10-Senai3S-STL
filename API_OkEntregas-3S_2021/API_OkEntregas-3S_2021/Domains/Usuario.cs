338fe5acd99f6b53808fdfc7c92a582a2d84d921
joaovitorfreitas <joaovdlf@gmail.com>
2021-10-22 03:45:14 -0300

    Login-Cadastro back

---
+++ API_OkEntregas-3S_2021/API_OkEntregas-3S_2021/Domains/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace API_OkEntregas_3S_2021.Domains
+{
+    public partial class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public bool TipoUsuario { get; set; }
+    }
+}
