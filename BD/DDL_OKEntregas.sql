c7c97de73a8c7f029e5ed1b093ccb6f2679e5e16
joaovitorfreitas <joaovdlf@gmail.com>
2021-10-28 02:19:05 -0300

    Banco de dados feitos

--- BD/DDL_OKEntregas.sql
+++ BD/DDL_OKEntregas.sql
@@ -1,6 +1,86 @@
+CREATE DATABASE OKEntregas
+
 USE OKEntregas
 
-INSERT INTO Usuarios	(Nome, Email, Senha, TipoUsuario)
-VALUES                  ('Daniel', 'daniel@gmail.com', '123', 0),
-						('Alan', 'alan@gmail.com', '123', 1)
-							
+Go
+
+CREATE TABLE Usuarios
+(
+		IdUsuario INT IDENTITY PRIMARY KEY NOT NULL,
+		Nome VARCHAR(200) NOT NULL,
+		Sobrenome VARCHAR(200) NOT NULL,
+		Email VARCHAR(200) UNIQUE NOT NULL,
+		Senha VARCHAR(200) NOT NULL,
+		TipoUsuario BIT NOT NULL
+);
+
+GO
+
+CREATE TABLE Empresa (
+		IdEmpresa INT IDENTITY PRIMARY KEY NOT NULL,
+		NomeEmpresa VARCHAR(255) NOT NULL,
+		NumeroDeFuncionarios INT NOT NULL ,
+		NumeroDeTelefone VARCHAR(13),	/* Tipo Int Mudado para varchar*/
+		EmailEmpresa VARCHAR(255) NOT NULL,
+		NomeFantasia VARCHAR(255),
+		Cnpj VARCHAR (14) NOT NULL,
+		SegmentoDeMercado VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE UsuarioLeads(
+		IdUsuarioLeads INT IDENTITY PRIMARY KEY NOT NULL,
+		Telefone VARCHAR(13) NOT NULL,
+		Email VARCHAR(255) NOT NULL,
+		statusLead VARCHAR(255) NOT NULL,
+		Cargo VARCHAR(255) NOT NULL,
+		Nome VARCHAR (255) NOT NULL 
+
+);
+
+GO
+
+CREATE TABLE Leads(
+		IdLeads INT IDENTITY PRIMARY KEY NOT NULL,
+		Score VARCHAR (100) NOT NULL,
+		Necessidades VARCHAR (255) NOT NULL,
+		IdUsuarioLeads INT FOREIGN KEY REFERENCES UsuarioLeads(IdUsuarioLeads) NOT NULL,
+		IdEmpresa INT FOREIGN KEY REFERENCES Empresa(IdEmpresa) NOT NULL 
+);
+
+GO
+
+CREATE TABLE Reuniao(
+		IdReuniao INT IDENTITY PRIMARY KEY NOT NULL,
+		DataInicio DATETIME NOT NULL, 
+		DataTermino DATETIME NOT NULL,
+		ATA VARCHAR(255),
+		IdUsuario INT FOREIGN KEY REFERENCES Usuarios(IdUsuario) NOT NULL,
+		IdLeads INT FOREIGN KEY REFERENCES Leads(IdLeads) NOT NULL,
+);
+
+GO
+
+CREATE TABLE WhatsAPP(
+		IdContato INT IDENTITY PRIMARY KEY NOT NULL,
+		IdLeads INT FOREIGN KEY REFERENCES Leads(IdLeads) NOT NULL,
+		NumeroContato  VARCHAR(12) NOT NULL,
+);
+
+GO
+
+CREATE TABLE Email(
+		IdEmail INT IDENTITY PRIMARY KEY NOT NULL,
+		IdLeads INT FOREIGN KEY REFERENCES Leads(IdLeads) NOT NULL,
+		Email  VARCHAR(255) NOT NULL,
+);
+
+GO
+
+CREATE TABLE Linkedin(
+		IdLinkedin INT IDENTITY PRIMARY KEY NOT NULL,
+		IdLeads INT FOREIGN KEY REFERENCES Leads(IdLeads) NOT NULL,
+		Nome VARCHAR(255) NOT NULL,
+		NumeroTelefone VARCHAR(255) NOT NULL,
+);
+
+
--- BD/DML_OKEntregas.sql
+++ BD/DML_OKEntregas.sql
@@ -1,12 +1,45 @@
-CREATE DATABASE OKEntregas
-
 USE OKEntregas
 
-CREATE TABLE Usuarios
-(
-		IdUsuario INT IDENTITY PRIMARY KEY NOT NULL,
-		Nome VARCHAR(200) NOT NULL,
-		Email VARCHAR(200) UNIQUE NOT NULL,
-		Senha VARCHAR(200) NOT NULL,
-		TipoUsuario BIT NOT NULL
-);
+INSERT INTO Usuarios	(Nome, Sobrenome, Email, Senha, TipoUsuario)
+VALUES                  ('Daniel', 'Ferreira', 'daniel@gmail.com', '123', 0),
+						('Alan', 'Malu', 'alan@gmail.com', '123', 1)
+
+
+INSERT INTO Empresa	(NomeEmpresa, NumeroDeFuncionarios, NumeroDeTelefone, EmailEmpresa, NomeFantasia, Cnpj, SegmentoDeMercado)
+VALUES                  ('EmpresaStl1', 12, 11971697877,'EmStl1@gmail.com', 'STL1', '123456789123', 'Tecnologia'),
+						('EmpresaStl2', 30, 11971697877,'EmStl2@gmail.com', 'STL2', '123456789123', 'Gastronomia'),
+						('EmpresaStl3', 40, 11971697877,'EmStl3@gmail.com', 'STL3', '123456789123', 'Eletrica')
+
+INSERT INTO UsuarioLeads	(Telefone, Email, statusLead, Cargo, Nome)
+VALUES                  ('1140028922', 'Kleber@gmail.com', 'ativo','Dev FullStack', 'Kleber'),
+						('1140028922', 'Alan@gmail.com', 'ativo','Dev FrontEnd', 'Alan'),
+						('1140028922', 'Caic@gmail.com', 'ativo','Dev BackEnd', 'Caic')
+
+INSERT INTO Leads	(Score, Necessidades, IdUsuarioLeads, IdEmpresa)
+VALUES                  ('Medio', 'Analista Sistema',  1, 1),
+						('Alto', 'Avaliador Industrial',  2, 2),
+						('Baixo', 'Gerente de marketing', 2, 3)
+
+							
+INSERT INTO Reuniao	(DataInicio, DataTermino, ATA, IdUsuario, IdLeads)
+VALUES                  ('2021-12-09 15:45:21' , '2021-12-09 16:00:08', 'FADWA1', 1, 1),
+						('2021-11-09 15:45:21' , '2008-11-09 16:00:40', 'FADWA2', 1, 3),
+						('2021-10-09 15:45:21' , '2008-10-09 16:00:21', 'FADWA3', 1, 2)
+
+
+INSERT INTO WhatsAPP	(IdLeads, NumeroContato)
+VALUES                  (1 , '1140028922'),
+						(2 , '1140028922'),
+						(3 , '1140028922')
+
+
+INSERT INTO Email	(IdLeads, Email)
+VALUES                  (1 , 'contato1@gmail.com'),
+						(2 , 'contato2@gmail.com'),
+						(3 , 'contato3@gmail.com')
+
+INSERT INTO Linkedin	(IdLeads, Nome, NumeroTelefone)
+VALUES                  (1 , 'Aldair', '1140028922'),
+						(2 , 'Alduar', '1140028922'),
+						(3 , 'Lua', '1140028922')
+							
