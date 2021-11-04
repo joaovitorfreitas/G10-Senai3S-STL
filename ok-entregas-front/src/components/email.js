9fe16013506d41bdf580be39be05f378ede49d70
Alan da Costa Santos <44711461817@info.local>
2021-11-04 16:57:27 -0300

    pesquisa home e side bar de email feitos

---
+++ ok-entregas-front/src/components/email.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+import '../css/base-style.css'
+import '../css/email.css'
+
+export default class Email extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+
+    render() {
+        return (
+            <section className="email-bar flex">
+                <h1>Enviar E-mail</h1>
+                <div>
+
+                </div>
+            </section>
+        )
+    }
+}
--- ok-entregas-front/src/pages/home.js
+++ ok-entregas-front/src/pages/home.js
@@ -8,6 +8,7 @@ import welcome from '../img/welcome.png'
 import '../css/base-style.css'
 import '../css/home.css'
 import Side from '../components/side-bar'
+import Email from '../components/email'
 
 export default class Login extends Component{
     constructor(props) {
@@ -21,9 +22,22 @@ export default class Login extends Component{
         return (
             <main>
                 <Side/>
+                <Email/>
                 <section className="content">
-                    <div className="glass-home">
-                    <h1>Home</h1>
+                    <div className="glass-home flex flex-collumn ai-flex-start jc-flex-start">
+                        <h1>Digite um CNPJ</h1>
+                        <form className="pesquisa flex">
+                            <div className="inputs-home flex flex-collumn">
+                                <label>CNPJ</label>
+                                <input type="text"></input>
+                            </div>
+                            <div className="flex ai-center ai-flex-end">
+                                <button className="btn-pesquisar flex ai-center jc-center"><i id="lupa" className="fas fa-search"></i>Buscar</button>
+                            </div>
+                        </form>
+                        <div className="resultado">
+
+                        </div>
                     </div>
                 </section>
                 <div className="bc1"></div>
