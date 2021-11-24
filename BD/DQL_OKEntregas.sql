3eb9764ff3663b28b93df66a64658e8c7cedfb10
DanielJJunior <daniel.souza.jr16@gmail.com>
2021-11-24 17:09:16 -0300

    update banco de dados

--- BD/DQL_OKEntregas.sql
+++ BD/DQL_OKEntregas.sql
@@ -6,7 +6,7 @@ FROM   Usuarios;
 SELECT IdEmpresa,  NomeEmpresa, NumeroDeFuncionarios, NumeroDeTelefone, EmailEmpresa, NomeFantasia, Cnpj, SegmentoDeMercado
 FROM   Empresa;
 
-SELECT IdUsuarioLeads,  Telefone, Email, statusLead, Cargo, Nome
+SELECT IdUsuarioLeads, Email, statusLead, Cargo, Nome
 FROM   UsuarioLeads;
 
 SELECT IdLeads,  Score, Necessidades,Telefone, Email, statusLead, Cargo , Nome,NomeEmpresa,NumeroDeFuncionarios, NumeroDeTelefone, EmailEmpresa, NomeFantasia,  Cnpj, SegmentoDeMercado
@@ -19,22 +19,6 @@ SELECT IdReuniao,  DataInicio, DataTermino, ATA, Sobrenome, Email, Senha, TipoUs
 FROM   Reuniao
 INNER JOIN  Usuarios ON Reuniao.IdUsuario = Usuarios.IdUsuario
 INNER JOIN  Leads ON  Leads.IdLeads =  Reuniao.IdLeads
-
-
-SELECT IdContato,  NumeroContato, Score, Necessidades, IdUsuarioLeads,IdEmpresa
-FROM   WhatsAPP
-INNER JOIN  Leads ON Leads.IdLeads = WhatsAPP.IdLeads
-
-SELECT IdEmail,  Email,Score, Necessidades, IdUsuarioLeads,IdEmpresa
-FROM   Email
-INNER JOIN  Leads ON Leads.IdLeads = Email.IdLeads
-
-
-SELECT IdLinkedin,  Nome, NumeroTelefone, Score, Necessidades, IdUsuarioLeads,IdEmpresa
-FROM   Linkedin
-INNER JOIN  Leads ON Leads.IdLeads = Linkedin.IdLeads
-
-
 
 
 
